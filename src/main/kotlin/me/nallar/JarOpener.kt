952d7fb10a6472823568a723751458aba3973cfb
Ross Allan <rallanpcl@gmail.com>
2016-01-19 12:51:32 +0000

    'it' instead of named parameters.

--- src/main/kotlin/me/nallar/JarOpener.kt
+++ src/main/kotlin/me/nallar/JarOpener.kt
@@ -4,18 +4,18 @@ import me.nallar.javatransformer.api.AccessFlags
 import me.nallar.javatransformer.api.JavaTransformer
 import java.io.File
 
-object JarOpener {
-	fun main(args: Array<String>) {
+public object JarOpener {
+	public fun main(args: Array<String>) {
 		if (args.size != 2) {
 			System.err.println("Usage: java -jar jaropener.jar input.jar output.jar")
 			return
 		}
 
 		val transformer = JavaTransformer()
-		transformer.addTransformer({ x ->
-			x.accessFlags({ it -> open(it) })
-			x.methods.forEach({ it.accessFlags({ open(it) }) })
-			x.fields.forEach({ it.accessFlags({ open(it) }) })
+		transformer.addTransformer({
+			it.accessFlags({ open(it) })
+			it.methods.forEach({ it.accessFlags({ open(it) }) })
+			it.fields.forEach({ it.accessFlags({ open(it) }) })
 		})
 
 		transformer.transformJar(File(args[0]), File(args[1]))
