1ba29f50e7c08992be18694c8234b8b4fcadd002
Ross Allan <ross@nallar.me>
2016-01-25 10:29:31 +0000

    Start 1.1-SNAPSHOT

--- src/main/kotlin/me/nallar/JarOpener.kt
+++ src/main/kotlin/me/nallar/JarOpener.kt
@@ -1,28 +1,27 @@
+@file:JvmName("JarOpener")
+
 package me.nallar
 
 import me.nallar.javatransformer.api.AccessFlags
 import me.nallar.javatransformer.api.JavaTransformer
-import java.io.File
+import java.nio.file.Paths
 
-public object JarOpener {
-	@JvmStatic
-	public fun main(args: Array<String>) {
-		if (args.size != 2) {
-			System.err.println("Usage: java -jar jaropener.jar input.jar output.jar")
-			return
-		}
-
-		val transformer = JavaTransformer()
-		transformer.addTransformer({
-			it.accessFlags({ open(it) })
-			it.methods.forEach({ it.accessFlags({ open(it) }) })
-			it.fields.forEach({ it.accessFlags({ open(it) }) })
-		})
-
-		transformer.transformJar(File(args[0]), File(args[1]))
+public fun main(args: Array<String>) {
+	if (args.size != 2) {
+		System.err.println("Usage: java -jar jaropener.jar input.jar output.jar")
+		return
 	}
 
-	private fun open(flag: AccessFlags): AccessFlags {
-		return flag.without(AccessFlags.ACC_FINAL).makeAccessible(true)
-	}
+	val transformer = JavaTransformer()
+	transformer.addTransformer({
+		it.accessFlags({ open(it) })
+		it.methods.forEach({ it.accessFlags({ open(it) }) })
+		it.fields.forEach({ it.accessFlags({ open(it) }) })
+	})
+
+	transformer.transform(Paths.get(args[0]), Paths.get(args[1]))
+}
+
+private fun open(flag: AccessFlags): AccessFlags {
+	return flag.without(AccessFlags.ACC_FINAL).makeAccessible(true)
 }
