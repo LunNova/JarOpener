8a25118d641041323eb27d0d58ea570214850b37
Ross Allan <ross@nallar.me>
2016-02-05 17:35:46 +0000

    Release version 1.2. Log errors but continue. Update to Kotlin RC.

--- src/main/kotlin/me/nallar/JarOpener.kt
+++ src/main/kotlin/me/nallar/JarOpener.kt
@@ -4,9 +4,10 @@ package me.nallar
 
 import me.nallar.javatransformer.api.AccessFlags
 import me.nallar.javatransformer.api.JavaTransformer
+import me.nallar.javatransformer.api.TransformationException
 import java.nio.file.Paths
 
-public fun main(args: Array<String>) {
+fun main(args: Array<String>) {
 	if (args.size != 2) {
 		System.err.println("Usage: java -jar jaropener.jar input.jar output.jar")
 		return
@@ -14,9 +15,13 @@ public fun main(args: Array<String>) {
 
 	val transformer = JavaTransformer()
 	transformer.addTransformer({
-		it.accessFlags({ open(it) })
-		it.methods.forEach({ it.accessFlags({ open(it) }) })
-		it.fields.forEach({ it.accessFlags({ open(it) }) })
+		try {
+			it.accessFlags({ open(it) })
+			it.members.forEach({ it.accessFlags({ open(it) }) })
+		} catch (e: TransformationException) {
+			System.err.println("Error transforming class: " + it.name)
+			e.printStackTrace()
+		}
 	})
 
 	transformer.transform(Paths.get(args[0]), Paths.get(args[1]))
